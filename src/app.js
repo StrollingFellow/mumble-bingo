323f98c82ab9aeb25f192da79c128f2ea395d51d
Kiva Hilgenberg <kivahilgenberg@gmail.com>
2021-01-19 22:02:37 -0700

    made randomizer script

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const rl = require('readlines');
-const app = express();
-const seedrandom = require('seedrandom');
 const fs = require('fs');
 const sass = require('sass');
 
 const port = 3000;
 const mumbleSass = sass.renderSync({file: `${__dirname}/views/style.scss` }).css;
 
+const squares = JSON.parse(fs.readFileSync(`${__dirname}/MumbleSquares.json`));
+
+const app = express();
 app.set('view engine', 'pug');
-app.set('views', `${__dirname}/views`)
+app.set('views', `${__dirname}/views`);
 
 app.get('/', async(req, res)=> {
-    const squares = JSON.parse(fs.readFileSync(`${__dirname}/MumbleSquares.json`));
-
     res.render("index", {squares: squares, styles: mumbleSass})
 });
 
